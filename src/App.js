c8ba7c54b6eca02cc4c8bc195abb0d718b81cff9
sarthaksavvy <bitfumes@gmail.com>
2020-06-19 08:58:16 +0530

    handle click event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from "react";
 import "./assets/css/style.css";
 
-function App({ title }) {
-  return (
-    <div>
-      <div className="bg-gray-600 text-white p-5 border">{title}</div>
-    </div>
-  );
-}
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { title: "Hello React 2" };
-//   }
-
-//   render() {
-//     return <div>{this.state.title}</div>;
-//   }
+// function App({ title }) {
+//   return (
+//     <div>
+//       <div className="bg-gray-600 text-white p-5 border">{title}</div>
+//     </div>
+//   );
 // }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "Hello React 2", isShowing: false };
+  }
+  // states are Immutable
+
+  render() {
+    return (
+      <section className="flex justify-center">
+        <div className="w-1/2">
+          <div className="text-center">
+            <div className="my-4">{this.state.title}</div>
+            <button
+              className="p-1 bg-blue-700 text-white my-2"
+              onClick={() =>
+                this.setState({ isShowing: !this.state.isShowing })
+              }
+            >
+              Toggle Image
+            </button>
+          </div>
+          {this.state.isShowing ? (
+            <img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80" />
+          ) : null}
+        </div>
+      </section>
+    );
+  }
+}
 
 export default App;

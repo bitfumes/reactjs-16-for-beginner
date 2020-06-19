adee287c7271b32ce384e6d2d171b4bc1b1c0c3d
sarthaksavvy <bitfumes@gmail.com>
2020-06-19 12:32:53 +0530

    react hook useState

--- src/App.js
+++ src/App.js
@@ -1,55 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./assets/css/style.css";
 import Images from "./components/Images";
 
-// function App({ title }) {
-//   return (
-//     <div>
-//       <div className="bg-gray-600 text-white p-5 border">{title}</div>
-//     </div>
-//   );
-// }
+function App() {
+  const [title, setTitle] = useState("Hello React");
+  const [isShowing, setIsShowing] = useState(false);
 
-class App extends React.Component {
-  constructor(props) {
-    console.log("App Contructor");
-
-    super(props);
-    this.state = { title: "Hello React 2", isShowing: false };
-  }
-  // states are Immutable
-  componentDidMount() {
-    console.log("App Mounted");
-    // this.setState({ title: "Hello LifeCycle" });
+  function handleClick() {
+    setIsShowing(!isShowing);
   }
 
-  componentDidUpdate() {
-    console.log("App Updated");
-  }
-
-  handleClick = () => {
-    this.setState({ isShowing: !this.state.isShowing });
-  };
-
-  render() {
-    console.log("App Render");
-    return (
-      <section className="flex justify-center">
-        <div className="w-1/2">
-          <div className="text-center">
-            <div className="my-4">{this.state.title}</div>
-            <button
-              className="p-1 bg-blue-700 text-white my-2"
-              onClick={this.handleClick}
-            >
-              Toggle Image
-            </button>
-          </div>
-          {this.state.isShowing ? <Images /> : null}
+  return (
+    <section className="flex justify-center">
+      <div className="w-1/2">
+        <div className="text-center">
+          <div className="my-4">{title}</div>
+          <button
+            className="p-1 bg-blue-700 text-white my-2"
+            onClick={handleClick}
+          >
+            Toggle Image
+          </button>
         </div>
-      </section>
-    );
-  }
+        {isShowing ? <Images /> : null}
+      </div>
+    </section>
+  );
 }
+
+// class App extends React.Component {
+//   // constructor(props) {
+//   //   console.log("App Contructor");
+
+//   //   super(props);
+//   //   this.state = { title: "Hello React 2", isShowing: false };
+//   // }
+//   // states are Immutable
+//   componentDidMount() {
+//     console.log("App Mounted");
+//     // this.setState({ title: "Hello LifeCycle" });
+//   }
+
+//   componentDidUpdate() {
+//     console.log("App Updated");
+//   }
+
+//   handleClick = () => {
+//     this.setState({ isShowing: !this.state.isShowing });
+//   };
+
+//   render() {
+//     console.log("App Render");
+//     return (
+//       <section className="flex justify-center">
+//         <div className="w-1/2">
+//           <div className="text-center">
+//             <div className="my-4">{this.state.title}</div>
+//             <button
+//               className="p-1 bg-blue-700 text-white my-2"
+//               onClick={this.handleClick}
+//             >
+//               Toggle Image
+//             </button>
+//           </div>
+//           {this.state.isShowing ? <Images /> : null}
+//         </div>
+//       </section>
+//     );
+//   }
+// }
 
 export default App;

9fbc3e033a282329a561caab8daeb505b5a5af6f
sarthaksavvy <bitfumes@gmail.com>
2020-06-19 13:43:10 +0530

    component inside component

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,44 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 export default function Images() {
-  //   const [myInterval, setmyInterval] = useState(null);
+  const Images = [
+    "https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80",
+    "https://images.unsplash.com/photo-1547111962-50a04ac74d73?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1506882741710-98761c574b48?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    "https://images.unsplash.com/photo-1487537177666-94b1f521631a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+  ];
 
-  useEffect(() => {
-    console.log("Images Comp Mounted");
-    const interval = setInterval(() => {
-      console.log("Hello");
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  function ShowImage() {
+    return Images.map((image) => {
+      return (
+        <div>
+          <img src={image} width="150" />
+        </div>
+      );
+    });
+  }
 
   return (
-    <img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80" />
+    <section>
+      <div className="flex justify-center">
+        <ShowImage />
+      </div>
+    </section>
   );
 }
-
-// export default class Images extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { interval: null };
-//   }
-//   componentDidMount() {
-//     console.log("Images Comp Mounted");
-//     this.setState({
-//       interval: setInterval(() => {
-//         console.log("Hello");
-//       }, 1000),
-//     });
-//   }
-
-//   componentWillUnmount() {
-//     console.log("Images Comp Unmounted");
-//     clearInterval(this.state.interval);
-//   }
-
-//   render() {
-//     return (
-//       <img src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80" />
-//     );
-//   }
-// }

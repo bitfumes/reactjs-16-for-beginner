81bfc8ba748db6cddd611bffb9a8288957b1e3d2
sarthaksavvy <bitfumes@gmail.com>
2020-06-19 08:33:52 +0530

    say hi to tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./assets/css/style.css";
 function App({ title }) {
   return (
     <div>
-      <div className="box">{title}</div>
+      <div className="bg-gray-600 text-white p-5 border">{title}</div>
     </div>
   );
 }

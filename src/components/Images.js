435d78497b02af3ad6215ea0ce6f2722eb524150
sarthaksavvy <bitfumes@gmail.com>
2020-08-31 00:53:07 +0530

    show images from unsplash API

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,51 +1,34 @@
 import React, { useEffect, useState, useRef, useLayoutEffect } from "react";
 import Image from "./image";
+import Axios from "axios";
 
 export default function Images() {
-  const [images, setimages] = useState([
-    "https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80",
-    "https://images.unsplash.com/photo-1547111962-50a04ac74d73?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1487537177666-94b1f521631a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1506882741710-98761c574b48?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-    "https://images.unsplash.com/photo-1487537177666-94b1f521631a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
-  ]);
+  const [images, setimages] = useState([]);
 
   const inputRef = useRef(null);
-  const varRef = useRef(images.length);
+
   useEffect(() => {
     inputRef.current.focus();
-    // console.log(varRef);
+    Axios.get(
+      "https://api.unsplash.com/photos/?client_id=8zkSPstt1cIVHrBRoCVx1dBWGhqryPF9Tl8WkvjI9pM"
+    ).then((res) => {
+      setimages(res.data);
+    });
   }, []);
-
-  useEffect(() => {
-    varRef.current = varRef.current + 1;
-  });
-
-  const [myName, setMyName] = useState("Sarthak");
-  useEffect(() => {
-    console.log("i am use effect 1");
-  });
-
-  useLayoutEffect(() => {
-    setMyName("Reactjs");
-    console.log("i am use effect 2");
-  });
 
   const [newImageUrl, setNewImageUrl] = useState("");
 
   function handleRemove(index) {
-    // setimages(images.filter((image, i) => i !== index));
     setimages([
       ...images.slice(0, index),
       ...images.slice(index + 1, images.length),
     ]);
-    console.log("I am changing state");
   }
 
   function ShowImage() {
     return images.map((img, index) => (
       <Image
-        image={img}
+        image={img.urls.regular}
         handleRemove={handleRemove}
         index={index}
         key={index}
@@ -66,8 +49,6 @@ export default function Images() {
 
   return (
     <section>
-      <p>My Name is {myName}</p>
-      <p>Component is updated {varRef.current} times</p>
       <div className="flex flex-wrap justify-center">
         <ShowImage />
       </div>

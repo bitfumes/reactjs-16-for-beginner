c9a1c5400adf900d37e836f4fb6f99b293e74eed
sarthaksavvy <bitfumes@gmail.com>
2020-09-02 01:28:09 +0530

    61 Firebase Login

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "../config/firebase";
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
   function handleForm(e) {
+    if (isLoading) return;
+    setIsLoading(true);
     e.preventDefault();
-    console.log("submitted");
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("sarthak@bitfumes.com", "pssword")
+      .then((res) => {
+        console.log(res);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setError(e.message);
+        setIsLoading(false);
+      });
   }
 
   return (
     <div className="flex h-screen bg-gray-200">
       <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
         <form className="m-5 w-10/12" onSubmit={handleForm}>
+          {error !== "" && <p>{error}</p>}
           <h1 className="w-full text-4xl tracking-widest text-center my-6">
             Login
           </h1>
@@ -32,7 +49,11 @@ export default function Login() {
               type="submit"
               className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-400 text-black"
             >
-              Login
+              {isLoading ? (
+                <i className="fas fa-circle-notch fa-spin"></i>
+              ) : (
+                "Login"
+              )}
             </button>
           </div>
         </form>

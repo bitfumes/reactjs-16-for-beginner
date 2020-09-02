d90217324c3155a1d3a9dd30ab0386858189bf38
sarthaksavvy <bitfumes@gmail.com>
2020-09-03 00:32:52 +0530

    68 Loading before fetch

--- src/App.js
+++ src/App.js
@@ -12,22 +12,29 @@ import firebase from "./config/firebase";
 import AppContext from "./store/AppContext";
 import AuthRoute from "./utils/routes/AuthRoute";
 import GuestRoute from "./utils/routes/GuestRoute";
+import Loading from "./components/Loading";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         setIsLoggedIn(true);
         setUser(user);
+        setIsLoading(false);
       } else {
         setUser({});
         setIsLoggedIn(false);
+        setIsLoading(false);
       }
     });
   }, []);
+
+  if (isLoading) return <Loading />;
 
   return (
     <Router>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex h-screen">
       <p className="m-auto">
-        <i className="fas fa-circle-notch fa-spin text-5xl text-yellow-400"></i>
+        <i className="fas fa-circle-notch fa-spin text-5xl text-blue-800"></i>
       </p>
     </div>
   );

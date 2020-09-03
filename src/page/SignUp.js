8bce2c92060d671f230af34589e92df71c475bb0
sarthaksavvy <bitfumes@gmail.com>
2020-09-04 00:51:24 +0530

    74 Formik Yup Validation

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import firebase from "../config/firebase";
 import { useHistory } from "react-router-dom";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function SignUp() {
   const formik = useFormik({
@@ -9,24 +10,10 @@ export default function SignUp() {
     onSubmit: (value) => {
       console.log("Formik", value);
     },
-    validate: (values) => {
-      const errors = {};
-      if (!values.email) {
-        errors.email = "Email Field is Required.";
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-      ) {
-        errors.email = "Email is invalid";
-      }
-
-      if (!values.password) {
-        errors.password = "Password Field is Required.";
-      } else if (values.password.length <= 6) {
-        errors.password = "Password must be longer than 6";
-      }
-
-      return errors;
-    },
+    validationSchema: Yup.object({
+      email: Yup.string().required("Email is required").email(),
+      password: Yup.string().required("Password is required").min(6),
+    }),
   });
 
   return (

3d6c01e437029efdf66884903e508a75ee2b0164
sarthaksavvy <bitfumes@gmail.com>
2020-09-04 00:32:01 +0530

    72 Getting Started With Formik

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -1,44 +1,20 @@
 import React, { useState } from "react";
 import firebase from "../config/firebase";
 import { useHistory } from "react-router-dom";
+import { useFormik } from "formik";
 
 export default function SignUp() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [form, setForm] = useState({ email: "", password: "" });
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const history = useHistory();
-
-  function handleForm(e) {
-    if (isLoading) return;
-    setIsLoading(true);
-    e.preventDefault();
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(form.email, form.password)
-      .then((res) => {
-        history.replace("/");
-        // setIsLoggedIn(true);
-        setError("");
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        setError(e.message);
-        setIsLoading(false);
-      });
-  }
-
-  function handleInput(e) {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  }
-
-  // if (isLoggedIn) return <Redirect to="/" />;
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    onSubmit: (value) => {
+      console.log("Formik", value);
+    },
+  });
 
   return (
     <div className="flex h-screen bg-gray-200">
       <div className="m-auto w-1/3 text-white flex flex-wrap justify-center shadow-lg rounded-lg bg-gradient-to-br from-indigo-900 to-indigo-700">
-        <form className="m-5 w-10/12" onSubmit={handleForm}>
-          {error !== "" && <p>{error}</p>}
+        <form className="m-5 w-10/12" onSubmit={formik.handleSubmit}>
           <h1 className="w-full text-4xl tracking-widest text-center my-6">
             Sign Up Here
           </h1>
@@ -48,8 +24,8 @@ export default function SignUp() {
               className="p-2 rounded shadow w-full text-black"
               placeholder="Email or Username"
               name="email"
-              value={form.email}
-              onChange={handleInput}
+              value={formik.values.email}
+              onChange={formik.handleChange}
             />
           </div>
           <div className="w-full my-6">
@@ -58,8 +34,8 @@ export default function SignUp() {
               className="p-2 rounded shadow w-full text-black"
               placeholder="password"
               name="password"
-              value={form.password}
-              onChange={handleInput}
+              value={formik.values.password}
+              onChange={formik.handleChange}
             />
           </div>
           <div className="w-full my-10">
@@ -67,11 +43,11 @@ export default function SignUp() {
               type="submit"
               className="p-2 rounded shadow w-full bg-gradient-to-tr from-yellow-600 to-yellow-400 text-black"
             >
-              {isLoading ? (
+              {/* {isLoading ? (
                 <i className="fas fa-circle-notch fa-spin"></i>
-              ) : (
-                "Login"
-              )}
+              ) : ( */}
+              Sign Up
+              {/* )} */}
             </button>
           </div>
         </form>

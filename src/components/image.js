fa9294125a6ae41a60ab614c9ee8acafff1b5e1f
sarthaksavvy <bitfumes@gmail.com>
2020-09-16 12:04:15 +0530

    78 Framer Motion image preview

--- src/components/image.js
+++ src/components/image.js
@@ -1,10 +1,12 @@
+import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 export default function Image({ index, image, handleRemove }) {
   const [isHovering, setIsHovering] = useState(false);
+  const [showPreview, setShowPreview] = useState(false);
 
   return (
-    <div className="w-1/3 p-1 border flex justify-center">
+    <div className="w-1/6 p-1 border flex justify-center">
       <div
         className="relative"
         onMouseEnter={() => setIsHovering(true)}
@@ -16,8 +18,24 @@ export default function Image({ index, image, handleRemove }) {
           }`}
           onClick={() => handleRemove(index)}
         ></i>
-        <img src={image} width="100%" height="auto" />
+        <img
+          onClick={() => setShowPreview(true)}
+          src={image}
+          width="100%"
+          height="auto"
+        />
       </div>
+
+      {showPreview && (
+        <section
+          className="fixed w-full h-full flex justify-center items-center top-0 left-0 z-40"
+          onClick={() => setShowPreview(false)}
+        >
+          <div className="bg-white">
+            <img src={image} className="rounded-lg" width="300" height="auto" />
+          </div>
+        </section>
+      )}
     </div>
   );
 }

cd8a1248d81a2a6757ff20d97c18a6928f9939a0
sarthaksavvy <bitfumes@gmail.com>
2020-09-16 11:35:57 +0530

    Framer Motion Animate text

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,9 +1,22 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 export default function Home() {
   return (
     <div className="flex h-screen">
-      <h1 className="m-auto text-3xl">Welcome Home</h1>
+      <motion.h1
+        initial={{ opacity: 0, y: -400 }}
+        animate={{
+          scale: 1.5,
+          opacity: 1,
+          y: 0,
+          transition: { duration: 1 },
+          rotate: 360,
+        }}
+        className="m-auto text-3xl"
+      >
+        Welcome Home
+      </motion.h1>
     </div>
   );
 }
